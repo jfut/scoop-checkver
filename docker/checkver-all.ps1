6ea34223ac95b2678a95c2c64d9ff6aef68701fa
Jun Futagawa <jfut@integ.jp>
2019-04-21 10:34:09 +0900

    Add support for checkver scripts

--- docker/checkver-all.ps1
+++ docker/checkver-all.ps1
@@ -1,10 +1,18 @@
+write-host -f DarkCyan "Running /scoop/bin/checkver-pre script..."
+& /scoop/bin/checkver-pre
+
 Get-ChildItem /scoop/buckets/ | ForEach-Object {
     $bucket = $_
     $checkver = "$bucket/bin/checkver.ps1"
     if (Test-Path $checkver) {
-        write-host -f DarkCyan "Check target: $bucket"
+        write-host -f DarkCyan "Target bucket: $bucket"
         write-host -f Yellow "Updating bucket..."
         & git --git-dir="$bucket/.git" pull
-        iex -command "$checkver"
+        write-host -f Yellow "Checking bucket..."
+        Invoke-Expression "pwsh $checkver | /scoop/bin/checkver-handle $bucket"
     }
 }
+
+write-host -f DarkCyan "Running /scoop/bin/checkver-post script..."
+& /scoop/bin/checkver-post
+
